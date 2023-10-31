dfd5e20e0fa2ffb92bd62a39403553b03cfdd69a
fernandozrzr <fernandozrzr@gmail.com>
2023-11-01 01:52:45 +0800

    Update Student and CampCommitteeMember

--- SC2002/src/sc2002/CampCommiteeMember.java
+++ SC2002/src/sc2002/CampCommiteeMember.java
@@ -1,5 +1,36 @@
 package sc2002;
 
-public class CampCommiteeMember {
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List; 
 
+public class CampCommiteeMember extends Student {
+    private int myPoints;
+
+    public CampCommiteeMember() {
+        myPoints = 0;
+    }
+
+    public void generateList(String filePath, String attendeeFilter, String campFilter, List<String> attendees, List<String> camps) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            // Write CSV header
+            writer.append("Attendee,Camp\n");
+
+            // Write data to the CSV file based on the filters
+            for (int i = 0; i < attendees.size(); i++) {
+                String attendee = attendees.get(i);
+                String camp = camps.get(i);
+
+                if (attendeeFilter==attendee&& campFilter==camp) {
+                    writer.append(attendee).append(",").append(camp).append("\n");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void viewPoints() {
+    	System.out.println("You have: "+ this.myPoints + " points.");
+    }
 }
--- SC2002/src/sc2002/Student.java
+++ SC2002/src/sc2002/Student.java
@@ -1,5 +1,23 @@
 package sc2002;
 
-public class Student {
+import java.util.ArrayList;
 
+public class Student extends User {
+	private ArrayList<Enquiries> myEnquiries;
+	private ArrayList<Camp> registeredCamps;
+	public Student() {
+		ArrayList<Enquiries> myEnquiries = new ArrayList<>();
+		ArrayList<Camp> registeredCamps = new ArrayList<>();
+	}
+	
+	public void viewMyEnquiries() {
+		for (Enquiries enquiry : myEnquiries) {
+            System.out.println(enquiry); 
+        }
+	}
+	public void viewRegisteredCamp() {
+		for (Camp camp : registeredCamps) {
+            System.out.println(camp);
+        }
+	}
 }
