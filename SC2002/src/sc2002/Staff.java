dae759fffa6432273ba1c44e67fa833413946829
kysx15 <131588512+kysx15@users.noreply.github.com>
2023-11-05 09:39:39 +0800

    Update Staff.java

--- SC2002/src/sc2002/Staff.java
+++ SC2002/src/sc2002/Staff.java
@@ -1,5 +1,23 @@
 package sc2002;
 
 public class Staff extends User {
-
+  private ArrayList<Camp> createdCamps;
+	
+	public Staff() {
+		ArrayList<Camp> createdCamps = new ArrayList<>();
+	}
+	
+	public String ViewMyCreatedCamp() {
+		for (Camp camp : createdCamps) {
+			System.out.println(camp);
+		}
+	}
+	
+	public void GenerateList(Camp) {
+		
+	}
+	
+	public void GenerateReport(Camp) {
+		
+	}
 }
