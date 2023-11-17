43c367f910bd9bd71c4253205f3be65bea895f3f
Edward095 <42329335+Edward095@users.noreply.github.com>
2023-11-17 18:08:03 +0800

    Updaed Camp view and student controller

--- SC2002/src/sc2002/StudentController.java
+++ SC2002/src/sc2002/StudentController.java
@@ -44,7 +44,17 @@ public class StudentController
                 {
                     // Show all the camps that student can view and options to view all details of the camp
                     ArrayList<Integer> campIndexes = view.DisplayAllCamps();
+                    System.out.print("Enter the index of the camp of the details you want to view(0 to go back): ");
+                    int index = input.nextInt() - 1;
 
+                    //Invalid Choice so exit out of case
+                    if(index < 0 || index > campIndexes.size())
+                    {
+                        System.out.println("You have entered an invalid choice. \n");
+                        break;
+                    }
+                    
+                    view.DisplayCampDetails(index);
                     break;
                 }
                 case 2: // Register For Camp
@@ -67,10 +77,16 @@ public class StudentController
                     switch(type)
                     {
                         case 1:
-                            //CampController.GetInstance().AddAttendee(campIndexes.get(index), user);
+                            if(camsApp.currentUser instanceof Student)
+                                CampController.GetInstance().AddAttendee(campIndexes.get(index), (Student)camsApp.currentUser);
+                            else
+                                System.out.println("Invalid Request");
                             break;
                         case 2:
-                            //CampController.GetInstance().AddCommitteeMember(campIndexes.get(index), user);
+                            if(camsApp.currentUser instanceof CCM)
+                                CampController.GetInstance().AddCommitteeMember(campIndexes.get(index), (CCM)camsApp.currentUser);
+                            else
+                                System.out.println("Invalid Request");
                             break;
                         default:
                             System.out.println("You have entered an invalid choice");
@@ -95,7 +111,7 @@ public class StudentController
                     System.out.print("Enter you enquiry: ");
                     String enquiry = input.next();
 
-                    EnquiriesController.GetInstance().AddEnquiry(index, enquiry, null);
+                    // AddEnquiry(index, (Student)camsApp.currentUser, enquiry);
                     break;
                 }
                 case 4: // View my enquires
@@ -105,12 +121,14 @@ public class StudentController
                 }
                 case 5: // View my profile
                 {
-                    // Show whether student is CCM
+                    // Show whether student is CCM, camps attended, Name, faculty
+                    // view.ViewProfile();
                     break;
                 }
                 case 6: // View registered camps
                 {
                     // Show camps that they registered for, roles (attendee or ccm) and option to withdraw from camp
+                    // view.ViewMyCamp();
                     break;
                 }
                 default:
