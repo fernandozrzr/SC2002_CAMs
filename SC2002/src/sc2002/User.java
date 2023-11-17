e379d924d8232bda0bfb108f61fc7cd841a7a0ba
QianYu0315 <144090066+QianYu0315@users.noreply.github.com>
2023-11-17 16:29:42 +0800

    Update User.java

--- SC2002/src/sc2002/User.java
+++ SC2002/src/sc2002/User.java
@@ -1,5 +1,38 @@
-package sc2002;
+package CAMS;
 
 public class User {
-    
+
+	protected String name;
+	protected String userID;
+	protected String faculty;
+	// protected String password;
+
+	public User(String name, String userID, String faculty) {
+		this.name = name;
+		this.userID = userID;
+		this.faculty = faculty;
+		// this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getUserID() {
+		return userID;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	/*
+	 * public String getPassword() { return password; }
+	 */
+
+	/*
+	 * public String toString() { return
+	 * String.format("Name: %s\r\nUserID: %s\r\nFaculty: %s\r\n", name, userID,
+	 * faculty); }
+	 */
 }
