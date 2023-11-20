880c7f8d6e058537b3b24bd0547ba77ffa545761
kysx15 <131588512+kysx15@users.noreply.github.com>
2023-11-20 11:40:41 +0800

    Update case 4-6
    still figuring out stuff sorry

--- SC2002/src/sc2002/StudentController.java
+++ SC2002/src/sc2002/StudentController.java
@@ -60,12 +60,12 @@ public class StudentController
                 case 2: // Register For Camp
                 {
                     // Show all camps with their respective slots available beside and option to register 
-                    ArrayList<Integer> campIndexes = view.DisplayAllCamps();
-                    System.out.print("Enter the index of the camp to register: ");
-                    int index = input.nextInt() - 1;
+                    ArrayList<Integer> campIDs = view.DisplayAllCamps();
+                    System.out.print("Enter the ID of the camp to register: ");
+                    int id = input.nextInt() - 1;
 
                     //Invalid Choice so exit out of case
-                    if(index < 0 || index > campIndexes.size())
+                    if(id < 0 || id > campIDs.size())
                     {
                         System.out.println("You have entered an invalid choice. \n");
                         break;
@@ -77,17 +77,21 @@ public class StudentController
                     switch(type)
                     {
                         case 1:
-                            if(camsApp.currentUser instanceof Student)
-                                CampController.GetInstance().AddAttendee(campIndexes.get(index), (Student)camsApp.currentUser);
+                            if(camsApp.currentUser instanceof Student) {
+                                CampController.GetInstance().AddAttendee(campIDs.get(id), (Student)camsApp.currentUser);
+                            	((Student) camsApp.currentUser).AddRegisteredCamps(id);
+                            	// SORRY still figuring this out: need to add a Camp object to myCamps
+                            }
                             else
                                 System.out.println("Invalid Request");
                             break;
                         case 2:
                             if(camsApp.currentUser instanceof Student) {
                             	Student s = (Student)camsApp.currentUser;
-                            	CCM currentUser = new CCM(0,s.GetRegisteredCamps(), s.getMyEnquiries());;
+                            	s.ccmID = campIDs.get(id);
+                            	CCM currentUser = new CCM(s.getName(), s.getUserID(), s.getFaculty(), 0, null);;
                             	camsApp.currentUser = currentUser;
-                            	CampController.GetInstance().AddCommitteeMember(campIndexes.get(index), (CCM)camsApp.currentUser);
+                            	CampController.GetInstance().AddCommitteeMember(campIDs.get(id), (CCM)camsApp.currentUser);
                             }
                             else
                                 System.out.println("Invalid Request");
@@ -100,39 +104,42 @@ public class StudentController
                 case 3: // Submit Enquires Regarding camp
                 {
                     // Show All camps and options to submit enquires
-                    ArrayList<Integer> campIndexes = view.DisplayAllCamps();
+                    ArrayList<Integer> campIDs = view.DisplayAllCamps();
 
-                    System.out.print("Enter the index of the camp to enquire: ");
-                    int index = input.nextInt() - 1;
+                    System.out.print("Enter the id of the camp to enquire: ");
+                    int id = input.nextInt() - 1;
 
                     //Check if the input is valid
-                    if(index < 0 || index > campIndexes.size())
+                    if(id < 0 || id > campIDs.size())
                     {
                         System.out.println("You have entered an invalid choice. \n");
                         break;
                     }
 
-                    System.out.print("Enter you enquiry: ");
+                    System.out.print("Enter your enquiry: ");
                     String enquiry = input.next();
-
+                    
+                    // SORRY still figuring the Enquiry stuff out
                     // AddEnquiry(index, (Student)camsApp.currentUser, enquiry);
                     break;
                 }
                 case 4: // View my enquires
                 {
                     // Show all of student's enquires and options to delete and edit them
+                	view.DisplayMyEnquiries(((Student)camsApp.currentUser).myEnquiries);
                     break;
                 }
                 case 5: // View my profile
                 {
-                    // Show whether student is CCM, camps attended, Name, faculty
-                    // view.ViewProfile();
+                    System.out.println("Name: " + camsApp.currentUser.getName() + "\n");
+                    System.out.println("ID: " + camsApp.currentUser.getUserID() + "\n");
+                    System.out.println("Faculty: " + camsApp.currentUser.getFaculty() + "\n");
                     break;
                 }
                 case 6: // View registered camps
                 {
                     // Show camps that they registered for, roles (attendee or ccm) and option to withdraw from camp
-                    // view.ViewMyCamp();
+                	view.DisplayMyCamps(((Student)camsApp.currentUser).registeredCamps);
                     break;
                 }
                 default:
