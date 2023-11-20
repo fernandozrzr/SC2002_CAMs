6eb41832ada52c60d6fc3a52db13bb7e818bfd7d
kysx15 <131588512+kysx15@users.noreply.github.com>
2023-11-20 19:45:06 +0800

    Updated functions for enquiries

--- SC2002/src/sc2002/Student.java
+++ SC2002/src/sc2002/Student.java
@@ -5,19 +5,13 @@ import java.util.ArrayList;
 public class Student extends User {
 	protected ArrayList<Enquiries> myEnquiries;
 	protected ArrayList<Camp> registeredCamps;
+	protected int ccmID;
 	
 	public Student(String name, String userID, String faculty){
 		super(name, userID, faculty);
 	}
 	
-	public ArrayList<Enquiries> GetMyEnquiries() {
-		return myEnquiries;
-    }
-
-	public void AddMyEnquiries(Enquiries enquiry){
-		myEnquiries.add(enquiry);
-	}
-	
+	// For camps
 	public ArrayList<Camp> GetRegisteredCamps() {
         	return registeredCamps;
     	}
@@ -25,4 +19,36 @@ public class Student extends User {
 	public void AddRegisteredCamps(Camp camp){
 		registeredCamps.add(camp);
 	}
+	
+	public void RemoveRegisteredCamps(Camp camp){
+		registeredCamps.remove(camp);
+	}
+	
+	// For enquiries
+	public ArrayList<Enquiries> GetMyEnquiries() {
+		return myEnquiries;
+    	}
+
+	public void AddMyEnquiry(Enquiries enquiry){
+		myEnquiries.add(enquiry);
+	}
+	
+	public void RemoveMyEnquiry(Enquiries enquiry){
+		myEnquiries.remove(enquiry);
+	}
+	
+	public void EditMyEnquiry(int index, String newEnquiry) {
+		myEnquiries.get(index).SetEnquiry(newEnquiry);
+	}
+	
+	public Enquiries FindEnquiry(int enquiryID) {
+		for (int i = 0; i < myEnquiries.size(); ++i) {
+			if (myEnquiries.get(i).GetEnquiryId() == enquiryID) 
+				return myEnquiries.get(i);
+		}
+		
+		System.out.println("Enquiry not found.");
+		return null;
+	}
+	
 }
