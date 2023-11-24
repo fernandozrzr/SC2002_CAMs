f401635540dae6e5bc89721892bb8299a6bed8b5
QianYu0315 <koid0001@e.ntu.edu.sg>
2023-11-24 22:52:00 +0800

    added comment for javadoc

--- SC2002/src/sc2002/User.java
+++ SC2002/src/sc2002/User.java
@@ -1,12 +1,35 @@
 package sc2002;
 
+/**
+ * User data structure (model)
+ * 
+ * @author koid qian yu
+ * @version 1.0
+ * @since 24/11/2023
+ */
 public class User {
 
+	/**
+	 * name of the user
+	 */
 	protected String name;
+	/**
+	 * unique user id  of the user
+	 */
 	protected String userID;
+	/**
+	 * faculty of the user
+	 */
 	protected String faculty;
-	// protected String password;
 
+	
+	/**
+	 * constructor of User class 
+	 * 
+	 * @param name name of the user
+	 * @param userID unique user id of the user
+	 * @param faculty faculty of the user
+	 */
 	public User(String name, String userID, String faculty) {
 		this.name = name;
 		this.userID = userID;
@@ -14,25 +37,31 @@ public class User {
 		// this.password = password;
 	}
 
+	/**
+	 * get the name of the user 
+	 * 
+	 * @return name of the user
+	 */
 	public String GetName() {
 		return name;
 	}
 
+	/**
+	 * get the user id of the user 
+	 * 
+	 * @return user id of the user
+	 */
 	public String GetUserID() {
 		return userID;
 	}
 
+	/**
+	 * get the faculty of the user 
+	 * 
+	 * @return faculty of the user
+	 */
 	public String GetFaculty() {
 		return faculty;
 	}
 
-	/*
-	 * public String getPassword() { return password; }
-	 */
-
-	/*
-	 * public String toString() { return
-	 * String.format("Name: %s\r\nUserID: %s\r\nFaculty: %s\r\n", name, userID,
-	 * faculty); }
-	 */
 }
