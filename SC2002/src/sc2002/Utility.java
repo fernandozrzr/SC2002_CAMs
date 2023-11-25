ccdaf0accc3e8bbd0a0ca0e3a899a0b721f7e9ac
Edward095 <42329335+Edward095@users.noreply.github.com>
2023-11-25 12:22:46 +0800

    Added Javadoc comments for Utility Class

--- SC2002/src/sc2002/Utility.java
+++ SC2002/src/sc2002/Utility.java
@@ -1,7 +1,21 @@
 package sc2002;
 
+/**
+ * Utility Class with static funtions to help make codes cleaner
+ * 
+ * @author Edward Seah
+ * @version 1.0
+ * @since 24/11/2023
+ */
 public class Utility 
 {
+    /**
+    * Checks if the string input from the user is a valid choice
+    * 
+    * @param userChoice User's input
+    * @param choices List of valid choices to check against userChoice
+    * @return True if user's input is valid, False otherwise
+    */
     public static boolean IsValidChoice(String userChoice, String[] choices)
     {
         for(String choice : choices)
